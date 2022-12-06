42d572b48673c9f2e2fc0280b3481b694ad3b59d
Arnau <aag84@bath.ac.uk>
2022-12-06 14:46:45 +0000

    Binned actions

---
+++ b_sim_seq.py
@@ -0,0 +1,151 @@
+# ! pip install mplsoccer
+
+import os
+import random
+import warnings
+
+import matplotlib.pyplot as plt
+import matplotsoccer as mps
+import numpy as np
+import pandas as pd
+import scipy
+import socceraction.atomic.spadl as atomicspadl
+import socceraction.spadl as spadl
+from mplsoccer import Pitch
+from scipy.ndimage import gaussian_filter
+from socceraction.data.statsbomb import StatsBombLoader
+from tqdm import tqdm
+
+import bin_action_seq as bs
+
+# Find similar sequences
+# Generate a heatmap for the set of similar actions
+# Then find what happens in all the next actionspip i
+
+
+
+warnings.filterwarnings('ignore')
+# Amount of bins to adjust heatmap
+X_B = bs.X_B
+Y_B = bs.Y_B
+M = bs.M
+REBUILD_DATA = False
+# Amount of start actions to check similarity
+ST = 4
+bin_data = bs.file_name
+
+def load_data():
+    
+    action_data = np.load(bin_data, allow_pickle=True)
+    seq_names = action_data.files
+    # print(len(seq_names))
+    for name in seq_names:
+        # print(name)
+        np_data = action_data[name]
+        # print(np_data)
+    return np_data
+
+def find_sim(np_data):
+    # Get second dimesion of array (bin number)
+    b = np.delete(np_data, [0,2], axis=2)
+    b = b.reshape((b.shape[0], b.shape[1] * b.shape[2]))
+    df_b = pd.DataFrame(b)
+
+    # Get (x,y) tuple for heatmap
+    x_y = np.delete(np_data, [0,1], axis=2)
+    x_y = x_y.reshape((x_y.shape[0], x_y.shape[1] * x_y.shape[2]))
+    df_xy = pd.DataFrame(x_y)
+
+    df_start = df_b.iloc[:, :ST]
+    # Get matching indices of duplicate rows
+    # df_start = df_start[df_start.duplicated(keep=False)]
+    matches = df_start.groupby(list(df_start)).apply(lambda x: tuple(x.index)).tolist()
+    # Sort list by length
+    matches.sort(key=len, reverse=True)
+
+    return matches,df_xy
+    
+def generate_hm(index,df_xy):
+    # df_match is the filtered df for matches
+
+    df_match = df_xy.iloc[df_xy.index.isin(index)]
+
+
+    # def generate_heatmap(np_data,index,name):
+    pitch = Pitch(pitch_type='statsbomb', line_zorder=2, pitch_color='#22312b', line_color='#efefef')
+    fig, ax = pitch.draw()
+
+
+    # Section to get x and y coordinates from df
+
+    start_seq = df_match.iloc[0, :8]
+    # Get x bin for all the actions, the x bin is the first number in the tuple
+    x = pd.Series([i[0] for i in start_seq])*(105/X_B)
+    y = pd.Series([i[1] for i in start_seq])*(68/Y_B)
+
+
+    # Convert our sequence to the bin_statistic format
+    stats = pitch.bin_statistic(x, y,bins = (X_B,Y_B), statistic='count')
+    # print(start_seq[0])
+    # # Plot start_seq as a heatmap in pitch
+    pcm = pitch.heatmap(stats, cmap='plasma', ax=ax)
+    pcm = pitch.heatmap(stats, cmap='plasma', ax=ax)
+    cbar = fig.colorbar(pcm, ax=ax, shrink=0.6)
+    cbar.outline.set_edgecolor('#efefef')
+    cbar.outline.set_linewidth(2)
+    # save the figure
+    dir_name = "./HM/{}/{}_{}_{}/".format(X_B*Y_B,len(index),ST,index[0])
+    results_dir = os.path.join(dir_name)
+    sample_file_name = "start_seq.png"
+    if not os.path.isdir(results_dir):
+        os.makedirs(results_dir)
+    fig.savefig(results_dir + sample_file_name, dpi=300, bbox_inches='tight', pad_inches=0.1)
+
+    for i in range(ST,len(df_match.columns)):
+        # For each subsequent action get all the x and y coordinates
+        x_i = pd.Series([i[0] for i in df_match.iloc[:, i]])*(105/X_B)
+        y_i = pd.Series([i[1] for i in df_match.iloc[:, i]])*(68/Y_B)
+        # Convert our sequence to the bin_statistic format
+        stats = pitch.bin_statistic(x_i, y_i,bins = (X_B,Y_B), statistic='count')
+        # print(start_seq[0])
+        # # Plot start_seq as a heatmap in pitch
+        pcm = pitch.heatmap(stats, cmap='plasma', ax=ax)
+        pcm = pitch.heatmap(stats, cmap='plasma', ax=ax)
+        # cbar = fig.colorbar(pcm, ax=ax, shrink=0.6)
+        # cbar.outline.set_edgecolor('#efefef')
+        # cbar.outline.set_linewidth(2)
+        # save the figure
+        sample_file_name = "start_seq_+{}.png".format(i+1)
+        
+        fig.savefig(results_dir + sample_file_name, dpi=300, bbox_inches='tight', pad_inches=0.1)
+
+if __name__ == '__main__':
+
+    # If file name exists, load it
+    if os.path.exists(bin_data) and not REBUILD_DATA:
+        np_data = load_data()
+    
+    # If bin file doesn't exist, create it
+    else:
+        # Call main method from bin_action_seq.py
+        bs.main()
+        np_data = load_data()
+    
+    print("Finding similar sequences")
+    # Add conditions like attacking half
+    matches, df_xy = find_sim(np_data)
+    # Sum the top 5 matches
+    top_5 = 0
+    for m in matches[:5]:
+        top_5 += len(m)
+    top_5 = top_5/5
+    print(X_B*Y_B)
+    print("Average length of top 5 matches: {}".format(top_5))
+    # print(matches[:5])
+
+
+    if len(matches) == 0:
+        print("No similar sequences")
+    # Generate heatmap for similar sequences
+    for i in range(len(matches[:3])) :        
+        generate_hm(matches[i],df_xy)
---
+++ bin_action_seq.py
@@ -0,0 +1,209 @@
+# Uncomment to install required modules
+
+# ! pip install mplsoccer --user
+# ! pip install socceraction --user
+# ! pip install statsbombpy
+
+import os
+import random
+import warnings
+
+import numpy as np
+import pandas as pd
+import socceraction.atomic.spadl as atomicspadl
+import socceraction.spadl as spadl
+from socceraction.data.statsbomb import StatsBombLoader
+from tqdm import tqdm
+
+USE_ATOMIC = False
+DOWNLOAD_RAW_DATA = False
+REBUILD_DATA = True
+X_B = 16
+Y_B = 12
+M = 12
+
+if not os.path.isdir('./seq'):
+        os.makedirs('./seq')
+file_name = './seq/b_u_data_seq_'+str(X_B)+'_'+str(M)+'.npz'
+warnings.filterwarnings('ignore')
+# Using API to get data from StatsBomb
+SBL = StatsBombLoader()
+# Get all the competion and season id's from the Barcelona games
+comps = SBL.competitions()
+comps = comps.loc[comps["competition_name"] == "La Liga"]
+comp_season_id = list(zip(comps.competition_id, comps.season_id))
+
+
+# Function to get all the game id's for a given competition and season
+def get_game_ids(comp_season_id):
+    all_games = []
+    for i in range(len(comp_season_id)):
+        comp_id = comp_season_id[i][0]
+        season_id = comp_season_id[i][1]
+        games = SBL.games(comp_id, season_id)
+        all_games += list(games.game_id)
+    return all_games
+
+
+
+# Function to get all actions for a given set of game id's
+def get_all_actions(games):
+    all_actions = []
+    for g in tqdm(games, total = len(games)):
+        events = SBL.events(g)
+        players = SBL.players(g)
+        players = players[['player_id', 'player_name', 'nickname']].drop_duplicates(subset='player_id')
+        teams = SBL.teams(g)
+        df_actions = spadl.statsbomb.convert_to_actions(events, home_team_id=777)
+        atomic_actions = atomicspadl.convert_to_atomic(df_actions)
+        atomic_actions = (
+        atomic_actions
+        .merge(atomicspadl.actiontypes_df(), how="left")
+        .merge(spadl.bodyparts_df(), how="left")
+        .merge(players, how="left")
+        .merge(teams, how="left")
+        )
+        atomic_actions["player_name"] = atomic_actions[["nickname", "player_name"]].apply(lambda x: x[0] if x[0] else x[1],axis=1)
+        del atomic_actions['nickname']
+        all_actions.append(atomic_actions)
+    df = pd.concat(all_actions)
+    return df
+
+
+# Create a list of n sequences
+# (n being a variable which is set to say 1000).
+# Where each sequence is created by picking a random time
+# Pick random time , random period , random game
+# Extracts the next to m actions (m being a variable,
+# say 10). Each sequence is saved as a numpy array with 3 columns
+# time (from first action), x, y position of the ball
+# at each subsequent action. In terms of matches lets extract them all
+# from the Barcelona games for now.
+# Save to single npz file
+
+def rolling_window(a, window, slide):
+    shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
+    strides = a.strides + (a.strides[-1],)
+    return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)[::slide]
+
+def seq_array(n=1000, m=10):
+
+    if USE_ATOMIC:
+        simple_actions_allowed = ["pass", "cross", "dribble", "shot", "take_on", "receival", "goal"]
+        x_var_name = 'x'
+        y_var_name = 'y'
+    else:
+        simple_actions_allowed = ["pass", "cross", "dribble", "shot"]
+        x_var_name = 'start_x'
+        y_var_name = 'start_y'
+
+    # check if file exists
+    if not os.path.isfile('pkl/barca_data_atomic.pkl') or DOWNLOAD_RAW_DATA:
+
+        all_games = get_game_ids(comp_season_id)
+
+        # Create a df of all games
+        # This is quicker than loading single random games afterwards
+        df = get_all_actions(all_games)
+
+        # Save to pickle
+        df.to_pickle('pkl/barca_data_atomic.pkl')
+
+    else:
+        df = pd.read_pickle("pkl/barca_data_atomic.pkl")
+
+    # # filter for Barcelona games
+    # Removed condition for now to check if Barcelona keep the ball
+    # df = df.loc[df["team_name"] == "Barcelona"]
+
+    # sort by game id and time
+    df = df.sort_values(by=["game_id", "period_id", "time_seconds"])
+
+    # Create a new column for the binned x and binned y
+    # Apply a scale to the binned columns
+    # Added 1 to so values can finish at 105 (end of pitch) 
+    # TO DO: condition for goals to be in correct bin
+    x_b = X_B
+    y_b = Y_B
+    df["x"] = (pd.cut(df["x"], bins=x_b, labels=False)+ 1) #* (105/x_b)
+    df["y"] = pd.cut(df["y"], bins=y_b, labels=False) #* (105/x_b)
+    # Create a tuple x and y
+    df["xy"] = list(zip(df["x"], df["y"]))
+    # Create new column for unique bin id
+    df["bin_id"] = df["x"] + (df["y"] * x_b)
+    
+    
+    team_allowed = ['Barcelona']
+    simple_actions_allowed = ["pass", "cross", "dribble", "shot", "take_on", "receival", "goal"]
+
+    # I feel like there is a way of doing this without a for loop !
+    sequences = []
+    for game_id, df_game in tqdm(df.groupby("game_id")):
+
+        # get all the time, x, y positions
+        # df_actions = df_game[["time_seconds", "type_name", "team_name", "x", "y"]]
+        df_actions = df_game[["time_seconds", "type_name", "team_name", "bin_id", "x", "y", "xy"]]
+        # create the sliding windows
+        a = rolling_window(np.arange(len(df_actions)), m, (np.rint(m / 2)).astype(int))
+
+        # Probably a better way to do this
+        df_test = pd.concat([pd.DataFrame(df_actions[x].to_numpy()[a]).rename(columns=lambda y: f'{x}_{y + 1}')
+                         for x in df_actions.columns], axis=1)
+
+
+        # get all column names that include "team_name_"
+        team_col_names = [col for col in df_test.columns if "team_name" in col]
+        action_col_names = [col for col in df_test.columns if "type_name" in col]
+
+        # Ensure only permitted actions and Barcelona keep the ball are included (and not the opposition) | Want to change this to be same team keeps the ball
+        df_test_filtered = df_test.loc[df_test[action_col_names].isin(simple_actions_allowed).all(axis=1) & df_test[team_col_names].isin(team_allowed).all(axis=1)]
+        # extract just the time, x, y columns
+        time_second_names = [col for col in df_test_filtered.columns if "time_seconds_" in col]
+        x_names = [col for col in df_test_filtered.columns if "x_" in col]
+        y_names = [col for col in df_test_filtered.columns if "y_" in col]
+        bin_id_names = [col for col in df_test_filtered.columns if "bin_id_" in col]
+        # Create a tuple of x and y positions
+        xy_names = [col for col in df_test_filtered.columns if "xy_" in col]
+        df_test_filtered = df_test_filtered[time_second_names + bin_id_names + xy_names]
+        
+        # convert every row of a dataframe to a numpy matrix of a given size - THIS IS DUMB WAY OF DOING IT
+        for index, row in df_test_filtered.iterrows():
+            action_matrix = row.to_numpy().reshape(3, m).transpose()
+
+            # TO DO - DO THIS OUTSIDE LOOP
+            time = action_matrix[0,0]
+            # subtract the time from the first action from all the actions
+            action_matrix[:, 0] = action_matrix[:, 0] - time
+
+            sequences.append(action_matrix)
+
+
+    # shuffle the sequences
+    random.shuffle(sequences)
+
+    if n < len(sequences):
+        sequences = sequences[:n]
+
+    # save to npz file
+    np.savez_compressed(file_name, sequences)
+
+def main():
+    random.seed(42)
+
+    # if file exists then load it
+    if not os.path.isfile(file_name) or REBUILD_DATA:
+        print("Creating data...")
+        seq_array(n=200000000, m=M)
+    else:
+        action_data = np.load(file_name)
+        seq_names = action_data.files
+        print(len(seq_names))
+        for name in seq_names:
+            print(name)
+            np_data = action_data[name]
+            print(np_data)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    main()
