1b0d5117ca8109f9ced0219d7d0c1be07e6ead4a
Arnau <aag84@bath.ac.uk>
2022-12-09 13:28:10 +0000

    Refactored binned sequence script

--- bin_action_seq.py
+++ bin_action_seq.py
@@ -15,12 +15,15 @@ import socceraction.spadl as spadl
 from socceraction.data.statsbomb import StatsBombLoader
 from tqdm import tqdm
 
+import sb_pipeline
+import wy_pipeline
+
 USE_ATOMIC = False
 DOWNLOAD_RAW_DATA = False
-REBUILD_DATA = False
+REBUILD_DATA = True
 X_B = 16
 Y_B = 12
-M = 4
+M = 12
 
 if not os.path.isdir('./seq'):
         os.makedirs('./seq')
@@ -28,46 +31,7 @@ file_name = './seq/b_u_data_seq_'+str(X_B)+'_'+str(M)+str(USE_ATOMIC)+'.npz'
 warnings.filterwarnings('ignore')
 # Using API to get data from StatsBomb
 SBL = StatsBombLoader()
-# Get all the competion and season id's from the Barcelona games
-comps = SBL.competitions()
-comps = comps.loc[comps["competition_name"] == "La Liga"]
-comp_season_id = list(zip(comps.competition_id, comps.season_id))
 
-
-# Function to get all the game id's for a given competition and season
-def get_game_ids(comp_season_id):
-    all_games = []
-    for i in range(len(comp_season_id)):
-        comp_id = comp_season_id[i][0]
-        season_id = comp_season_id[i][1]
-        games = SBL.games(comp_id, season_id)
-        all_games += list(games.game_id)
-    return all_games
-
-
-
-# Function to get all actions for a given set of game id's
-def get_all_actions(games):
-    all_actions = []
-    for g in tqdm(games, total = len(games)):
-        events = SBL.events(g)
-        players = SBL.players(g)
-        players = players[['player_id', 'player_name', 'nickname']].drop_duplicates(subset='player_id')
-        teams = SBL.teams(g)
-        df_actions = spadl.statsbomb.convert_to_actions(events, home_team_id=777)
-        atomic_actions = atomicspadl.convert_to_atomic(df_actions)
-        atomic_actions = (
-        atomic_actions
-        .merge(atomicspadl.actiontypes_df(), how="left")
-        .merge(spadl.bodyparts_df(), how="left")
-        .merge(players, how="left")
-        .merge(teams, how="left")
-        )
-        atomic_actions["player_name"] = atomic_actions[["nickname", "player_name"]].apply(lambda x: x[0] if x[0] else x[1],axis=1)
-        del atomic_actions['nickname']
-        all_actions.append(atomic_actions)
-    df = pd.concat(all_actions)
-    return df
 
 
 # Create a list of n sequences
@@ -77,8 +41,7 @@ def get_all_actions(games):
 # Extracts the next to m actions (m being a variable,
 # say 10). Each sequence is saved as a numpy array with 3 columns
 # time (from first action), x, y position of the ball
-# at each subsequent action. In terms of matches lets extract them all
-# from the Barcelona games for now.
+# at each subsequent action. 
 # Save to single npz file
 
 def rolling_window(a, window, slide):
@@ -86,8 +49,12 @@ def rolling_window(a, window, slide):
     strides = a.strides + (a.strides[-1],)
     return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)[::slide]
 
-def seq_array(n=1000, m=10):
+def seq_array(df,n=150000, m=10):
 
+    
+    # sort by game id and time
+    df = df.sort_values(by=["game_id", "period_id", "time_seconds"])
+    
     if USE_ATOMIC:
         simple_actions_allowed = ["pass", "cross", "dribble", "shot", "take_on", "receival", "goal"]
         x_var_name = 'x'
@@ -97,60 +64,27 @@ def seq_array(n=1000, m=10):
         x_var_name = 'start_x'
         y_var_name = 'start_y'
 
-    # check if file exists
-    # Change this for to apply for all .pkl files
-    if not os.path.isfile('barca_data_atomic.pkl') or DOWNLOAD_RAW_DATA:
-
-        print("Downloading raw data from StatsBomb")
-        all_games = get_game_ids(comp_season_id)
-
-        # Create a df of all games
-        # This is quicker than loading single random games afterwards
-        df = get_all_actions(all_games)
-
-        # Save to pickle
-        df.to_pickle('pkl/barca_data_atomic.pkl')
-
-    else:
-        # df = pd.read_pickle("pkl/barca_data_atomic.pkl")
-        print("Loading data from pickle")
-        sb_df = pd.read_pickle("../pkl_data/sb_all_data.pkl")
-        wy_df = pd.read_pickle("../pkl_data/wy_all_data.pkl")
-        df = pd.concat([sb_df, wy_df])
-
-    # # filter for Barcelona games
-    # Removed condition for now to check if Barcelona keep the ball
-    # df = df.loc[df["team_name"] == "Barcelona"]
-
-    # sort by game id and time
-    df = df.sort_values(by=["game_id", "period_id", "time_seconds"])
-
-    # Create a new column for the binned x and binned y
-    # Apply a scale to the binned columns
-    # Added 1 to so values can finish at 105 (end of pitch) 
-    # TO DO: condition for goals to be in correct bin
-    x_b = X_B
-    y_b = Y_B
-    df["x"] = (pd.cut(df[x_var_name], bins=x_b, labels=False)+ 1) #* (105/x_b)
-    df["y"] = pd.cut(df[y_var_name], bins=y_b, labels=False) #* (105/x_b)
+    df[x_var_name] = (pd.cut(df[x_var_name], bins=X_B, labels=False)+ 1) #* (105/x_b)
+    df[y_var_name] = pd.cut(df[y_var_name], bins=Y_B, labels=False) #* (105/x_b)
     # Create a tuple x and y
-    df["xy"] = list(zip(df["x"], df["y"]))
+    df["xy"] = list(zip(df[x_var_name], df[y_var_name]))
     # Create new column for unique bin id
-    df["bin_id"] = df["x"] + (df["y"] * x_b)
-    
-    
-    team_allowed = ['Barcelona']
-    simple_actions_allowed = ["pass", "cross", "dribble", "shot", "take_on", "receival", "goal"]
-
+    df["bin_id"] = df[x_var_name] + (df[y_var_name] * X_B)
     # I feel like there is a way of doing this without a for loop !
     sequences = []
+
     for game_id, df_game in tqdm(df.groupby("game_id")):
 
+        if USE_ATOMIC:
+            # filter out rows where dx is zero and dy is zero
+            df_game = df_game.loc[(df_game["dx"] != 0) | (df_game["dy"] != 0)]
+
         # get all the time, x, y positions
-        # df_actions = df_game[["time_seconds", "type_name", "team_name", "x", "y"]]
-        df_actions = df_game[["time_seconds", "type_name", "team_id", "bin_id", "x", "y", "xy"]]
+        # df_actions = df_game[["time_seconds", "type_name", "team_id", x_var_name, y_var_name]]
+        df_actions = df_game[["time_seconds", "type_name", "team_id", "bin_id", x_var_name, y_var_name, "xy"]]
+
         # create the sliding windows
-        a = rolling_window(np.arange(len(df_actions)), m, (np.rint(m / 2)).astype(int))
+        a = rolling_window(np.arange(len(df_actions)), m, (np.rint(m / 2)).astype(int)-1)
 
         # Probably a better way to do this
         df_test = pd.concat([pd.DataFrame(df_actions[x].to_numpy()[a]).rename(columns=lambda y: f'{x}_{y + 1}')
@@ -161,29 +95,46 @@ def seq_array(n=1000, m=10):
         team_col_names = [col for col in df_test.columns if "team_id" in col]
         action_col_names = [col for col in df_test.columns if "type_name" in col]
 
-        # Ensure only permitted actions and Barcelona keep the ball are included (and not the opposition) | Want to change this to be same team keeps the ball
-        # df_test_filtered = df_test.loc[df_test[action_col_names].isin(simple_actions_allowed).all(axis=1) & df_test[team_col_names].isin(team_allowed).all(axis=1)]
+        # Ensure only permitted actions and Barcelona keep the ball are included 
         df_test_filtered = df_test.loc[df_test[action_col_names].isin(simple_actions_allowed).all(axis=1) & df_test[team_col_names].nunique(axis=1) == 1]
+
         # extract just the time, x, y columns
         time_second_names = [col for col in df_test_filtered.columns if "time_seconds_" in col]
-        x_names = [col for col in df_test_filtered.columns if "x_" in col]
-        y_names = [col for col in df_test_filtered.columns if "y_" in col]
+
+        x_names = [col for col in df_test_filtered.columns if x_var_name+"_" in col]
+        y_names = [col for col in df_test_filtered.columns if y_var_name+"_" in col]
         bin_id_names = [col for col in df_test_filtered.columns if "bin_id_" in col]
-        # Create a tuple of x and y positions
         xy_names = [col for col in df_test_filtered.columns if "xy_" in col]
-        df_test_filtered = df_test_filtered[time_second_names + bin_id_names + xy_names]
-        
+        # team_ids = [col for col in df_test_filtered.columns if "team_id_" in col]
+
+        df_test_filtered = df_test_filtered[time_second_names + bin_id_names + xy_names ]
+
+
         # convert every row of a dataframe to a numpy matrix of a given size - THIS IS DUMB WAY OF DOING IT
         for index, row in df_test_filtered.iterrows():
-            action_matrix = row.to_numpy().reshape(3, m).transpose()
 
+
+            action_matrix = row.to_numpy().reshape(3, m).transpose()
+            # Condition to ensure that the sequence is only of same team
+            # Check all the items in 4th column are the same
+            # if len(np.unique(action_matrix[:, 3])) != 1:
+            #     # If they are not the same, then the ball has passed to the opposition
+            #     continue
+            # Drop 4th column which is team name
+            action_matrix = action_matrix[:, :3]
             # TO DO - DO THIS OUTSIDE LOOP
             time = action_matrix[0,0]
             # subtract the time from the first action from all the actions
-            action_matrix[:, 0] = action_matrix[:, 0] - time
+            action_matrix[:,0] -= time
+
+            # FUDGE TO ENSURE THAT YOU DON'T GET IDENTICAL TIMES
+            vpairs = (action_matrix[:-1, 0] == action_matrix[1:, 0])# append to sequences
+            vpairs = np.append(False, vpairs)
+            # for a masked array, add random values to the masked values
+            # this is to ensure that the time is not zero
+            action_matrix[:, 0] = np.ma.masked_array(action_matrix[:, 0], mask=vpairs).filled(action_matrix[:, 0] + np.random.uniform(0.0, 0.5))
 
             sequences.append(action_matrix)
-
 
     # shuffle the sequences
     random.shuffle(sequences)
@@ -197,19 +148,46 @@ def seq_array(n=1000, m=10):
 def main():
     random.seed(42)
 
-    # if file exists then load it
+    # # if file exists then load it
+    # if not os.path.isfile(file_name) or REBUILD_DATA:
+    #     print("Creating data...")
+    #     seq_array(n=200000000000, m=M)
+    # else:
+    #     action_data = np.load(file_name)
+    #     seq_names = action_data.files
+    #     print(len(seq_names))
+    #     for name in seq_names:
+    #         print(name)
+    #         np_data = action_data[name]
+    #         print(np_data)
     if not os.path.isfile(file_name) or REBUILD_DATA:
-        print("Creating data...")
-        seq_array(n=200000000, m=M)
+        sb_df = pd.DataFrame()
+        wy_df = pd.DataFrame()
+        # Importing SB data
+        print("Importing SB data")
+        sb = sb_pipeline.SBPipeline()
+        sb_df = sb.run_pipeline()
+
+        # Importing Wyscout data
+        wy = wy_pipeline.wyPipeline()
+        print("Importing Wyscout data")
+        wy_df = wy.run_pipeline()
+        
+        # Concatenate the two dataframes
+        df = pd.concat([sb_df, wy_df])
+        
+        random.seed(42)
+        print("Creating sequences...")
+        seq_array(df, n=150000, m=M)
     else:
-        action_data = np.load(file_name)
+        print("Loading existing data_seq.npz file!")
+        action_data = np.load(file_name,allow_pickle=True)
         seq_names = action_data.files
         print(len(seq_names))
         for name in seq_names:
             print(name)
             np_data = action_data[name]
             print(np_data)
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
